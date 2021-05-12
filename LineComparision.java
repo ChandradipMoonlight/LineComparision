761fd6c7db2f0dfb47a57eca12fc5ef8a0c0f0d1
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-12 15:41:55 +0530

    greeting messge added

---
+++ LineComparision.java
@@ -0,0 +1,13 @@
+package linedistance;
+
+public class LineComparision {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Line comparision computation Program");
+		
+	
+		
+
+	}
+
+}
