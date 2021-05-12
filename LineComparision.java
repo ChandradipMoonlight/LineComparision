01de98a538322b66544235eccdb5a57e59aa6b09
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-12 20:55:42 +0530

    Checkng equality of two lines

--- LineComparision.java
+++ LineComparision.java
@@ -18,7 +18,26 @@ public class LineComparision {
 		
 		double distanceLine1=Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
 		
+		System.out.println("Enter the values next co-ordinates ");
+		System.out.println("x3= ");
+		int x3 = sc.nextInt();
+		System.out.println("y3");
+		int y3 = sc.nextInt();
+		System.out.println("x4= ");
+		int x4 = sc.nextInt();
+		System.out.println("y4= ");
+		int y4 = sc.nextInt();
+		
+		double distanceLine2=Math.sqrt(Math.pow(x4-x3,2) + Math.pow(y4-y3,2));
+		
 		System.out.println("lenth of line1= "+ distanceLine1);
+		System.out.println("Length of line 2 = "+ distanceLine2);
+		
+		if (distanceLine1==distanceLine2) {
+			System.out.println("Two lines are Equal");
+		} else {
+			System.out.println("Two lines are not Euqal");
+		}
 		
 
 	}
