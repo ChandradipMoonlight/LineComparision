c3e13724bf490807209031a8346c24885e48f707
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-17 18:42:49 +0530

    refactored code using class and objects

--- LineComparision.java
+++ LineComparision.java
@@ -1,6 +1,4 @@
 package linedistance;
-//import java.util.Scanner;
-
 
 class DistanceOfLine{
 	
@@ -9,23 +7,19 @@ public int x1, x2, y1, y2;
 
 
 public  DistanceOfLine(int x1, int x2, int y1, int y2) {
-	 this.x1=x1;
-	 this.x2=x2;
-	 this.y1=y1;
-	 this.y2=y2;
+	 this.x1 = x1;
+	 this.x2 = x2;
+	 this.y1 = y1;
+	 this.y2 = y2;
 	 
 }
-
 
 public double calculateDistanceOfLine() {
 	double distanceLine = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
 
 	return distanceLine;
 }
-
-
 }
-
 
 public class LineComparision {
 
@@ -44,8 +38,6 @@ public class LineComparision {
 	public static void main(String[] args) {
 		System.out.println("Wellcome to the Line comparision Computatation problem");
 		
-		
-		
 		DistanceOfLine line1 = new DistanceOfLine(5, 8, 6, 9);
 	
 		double length1 = line1.calculateDistanceOfLine();	
@@ -57,8 +49,6 @@ public class LineComparision {
 		double length2 = line2.calculateDistanceOfLine();	
 		System.out.println("Length of line-2 = "+length2);
 		
-		diffOfLine(length1, length2);
-			
-		
+		diffOfLine(length1, length2);	
 }
 }
